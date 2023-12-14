ccb8e293a6536ed544793eb67cb7d9499ac7bc34
Daniel Kolyada <danilkolyada777@gmail.com>
2023-12-14 20:09:09 +0300

    Lectures

---
+++ service/MainClass.java
@@ -0,0 +1,10 @@
+package MTS.service;
+
+import MTS.dto.House;
+
+public class MainClass {
+    public static void main(String[] args) {
+        House woodHouse = new House();
+        woodHouse.setMaterial("Wood");
+    }
+}
